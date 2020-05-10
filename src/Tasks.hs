8cdfc5e480fd3d8e009377e4cbfb6bfed439ddf0
Volkov Ivan <volkov.ivan2004@gmail.com>
2020-05-10 19:27:31 +0300

    Second Part Volkov Ivan

--- src/Tasks.hs
+++ src/Tasks.hs
@@ -28,8 +28,8 @@ drop' n [] = []
 drop' n (a:as) = drop' (n - 1) as
 
 -- 5. filter' возвращает список из элементов, для которых f возвращает True
-check' True a = [a]
-check' False a = []
+--check' True a = [a]
+--check' False a = []
 filter'' f False (a:as) = filter' (f) (as)
 filter'' f True (a:as) = a : filter' (f) (as)
 
@@ -40,25 +40,33 @@ filter' f (a:as) = filter'' f (f a) (a:as) --(check' (f a) (a)) filter' (f) (as)
 -- 6. zip' принимает два списка [a1, a2, ...] и [b1, b2, ...] и возвращает список [(a1, b1), (a2, b2), ...].
 -- Размер итогого списка равен размеру меньшего из входных списков.
 zip' :: [a] -> [b] -> [(a, b)]
-zip' = undefined
+zip' [] [] = []
+zip' [] as = []
+zip' as [] = []
+zip' (a:as) (b:bs) = (a, b) : zip' as bs
 
 -- 7. map' принимает на вход функцию и список и применяет функцию ко всем элементам списка
 map' :: (a -> b) -> [a] -> [b]
-map' = undefined
+map' f [] = []
+map' f (a:as) = (f a) : map' f as
 
 -- 8. foldr' последовательно применяет функцию f с конца
 -- foldr' (+) 0 [1, 2, 3] == (1 + (2 + (3 + 0)))
 -- foldr' (*) 4 [] == 4
 foldr' :: (b -> a -> a) -> a -> [b] -> a
-foldr' = undefined
+foldr' f a [] = a
+foldr' f a (b:bs) = f b (foldr' f a bs)
 
 -- 9. foldl' последовательно применяет функцию f с начала
 -- foldl' (+) 0 [1, 2, 3] == (((0 + 1) + 2) + 3)
 -- foldl' (*) 4 [] == 4
 foldl' :: (a -> b -> a) -> a -> [b] -> a
-foldl' = undefined
+foldl' f a [] = a
+foldl' f a (b:bs) = foldl' f (f a b) bs 
+
 
 -- 10. concat' принимает на вход два списка и возвращает их конкатенацию
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' = undefined
+concat' [] as = as
+concat' (a:as) bs = a : concat' as bs
