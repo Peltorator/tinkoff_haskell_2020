01219147f1347ad14014ba45b754ae8509d6101a
Semyon Savkin <savkinsd2089@gmail.com>
2020-05-23 01:33:37 +0300

    solved tasks2

--- src/Tasks2.hs
+++ src/Tasks2.hs
@@ -11,32 +11,36 @@ module Tasks2 where
 -- то есть если на входе был список [[x1, x2, x3, x4], [y1, y2, y3], [z1, z2, z3, z4, z5]],
 -- то итоговым результатом должен быть сисок [x1 + y1 + z1, x2 + y2 + z2, x2 + y3 + z3].
 sum_of_lists :: Num a => [[a]] -> [a]
-sum_of_lists = undefined
+sum_of_lists = foldr (zipWith (+)) (repeat 0)
 
 -- 12. n'thPrime принимает на вход число n >= 0 и возвращает n-е простое число.
 n'th_prime :: Int -> Int
-n'th_prime = undefined
+n'th_prime = (primes !!)
+    where primes = [x | x <- [2..], null [d | d <- [2..(floor (sqrt (fromIntegral x)))], x `mod` d == 0]]
 
 -- 13. tails' возвращает все хвосты входного списка,
 -- то есть если на входе был список [1, 2, 3],
 -- то итоговым результатом должен быть список [[1, 2, 3], [2, 3], [3], []]
 -- Необходимо реализовать функцию при помощи foldr.
 tails' :: [a] -> [[a]]
-tails' = undefined
+tails' = foldr updateTails [[]]
+    where updateTails val list = (val:head list):list
 
 -- 14. inits' возвращает все префиксы входного списка,
 -- то есть если на входе был список [1, 2, 3],
 -- то итоговым результатом должен быть список [[], [1], [1, 2], [1, 2, 3]]
 -- Необходимо реализовать функцию при помощи foldr.
 inits' :: [a] -> [[a]]
-inits' = undefined
+inits' = foldr updateInits [[]]
+    where updateInits val list = ([] : (map ((:) val) list))
 
 -- 15. reverse' переворачивает список, который был дан на входе.
 -- Необходимо реализовать функцию при помощи foldr.
 reverse' :: [a] -> [a]
-reverse' = undefined
+reverse' = foldr pushBack []
+    where pushBack val list = list ++ [val]
 
 -- 16. reverse'' переворачивает список, который был дан на входе.
 -- Необходимо реализовать функцию при помощи foldl.
 reverse'' :: [a] -> [a]
-reverse'' = undefined
+reverse'' = foldl (flip (:)) []
