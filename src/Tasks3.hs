0c658f21002d51fbf0fbd37527faa646d4289297
AlexanderNekrasov <34592341+AlexanderNekrasov@users.noreply.github.com>
2020-05-28 03:14:02 +0300

    src/Tasks3.hs written

--- src/Tasks3.hs
+++ src/Tasks3.hs
@@ -12,7 +12,12 @@ data Person = Person { firstName :: String, lastName :: String, age :: Int }
 -- Однако если имя было короче двух символов, то оно не меняется.
 -- P.S. Если что, строка -- список символов.
 abbrFirstName :: Person -> Person
-abbrFirstName = undefined
+abbrFirstName p = Person (let name = firstName p in
+                              if length name > 1
+                              then head name : "."
+                              else name)
+                         (lastName p) 
+                         (age p)
 
 
 -- Определим наше дерево, которое мы далее будем использовать:
@@ -22,17 +27,22 @@ data Tree a = Nil | Node (Tree a) a (Tree a)
 
 -- 18. Функция treeSum вычисляет сумму элементов дерва.
 treeSum :: Tree Integer -> Integer
-treeSum = undefined
+treeSum Nil                   = 0
+treeSum (Node left val right) = treeSum left + val + treeSum right
 
 
 -- 19. Функция treeHeight вычисляет максимальную высоту дерева.
 treeHeight :: Tree a -> Int
-treeHeight = undefined
+treeHeight Nil                 = 0
+treeHeight (Node left _ right) = max (treeHeight left) (treeHeight right) + 1
 
 
 -- 20. Сделайте Tree представителем класса типов Eq.
 instance Eq a => Eq (Tree a) where
-    (==) = undefined
+    (==) Nil Nil                                           = True
+    (==) Nil _                                             = False
+    (==) _ Nil                                             = False
+    (==) (Node left1 val1 right1) (Node left2 val2 right2) = (val1 == val2) && (left1 == left2) && (right1 == right2)
 
 -- Для реализации свертки двоичных деревьев нужно выбрать алгоритм обхода узлов дерева.
 -- Сделайте дерево представителем класса типов Foldable несколькими способами.
@@ -52,23 +62,29 @@ newtype Levelorder a = LevelO (Tree a) deriving (Eq, Show)
 --
 -- 21.
 instance Foldable Tree where
-    foldr = undefined
+    foldr f init Nil                   = init
+    foldr f init (Node left val right) = foldr f (f val (foldr f init right)) left
 
 -- 22.
 instance Foldable Preorder where
-    foldr = undefined
+    foldr f init (PreO Nil)                   = init
+    foldr f init (PreO (Node left val right)) = f val (foldr f (foldr f init (PreO right)) (PreO left))
 
 -- 23.
 instance Foldable Postorder where
-    foldr = undefined
+    foldr f init (PostO Nil)                   = init
+    foldr f init (PostO (Node left val right)) = foldr f (foldr f (f val init) (PostO right)) (PostO left)
 
 -- 24.
+
 instance Foldable Levelorder where
-    foldr = undefined
+    foldr f init (LevelO t) = foldr f init (concat (levels t))
+        where levels Nil                   = [[]]
+              levels (Node left val right) = [val] : take (max (treeHeight left) (treeHeight right)) (zipWith (++) (levels left ++ repeat []) (levels right ++ repeat []))
 
 -- 25. treeSum' вычисляет сумму элементов дерева. Примените foldr.
 treeSum' :: Tree Integer -> Integer
-treeSum' = undefined
+treeSum' = foldr (+) 0
 
 
 
@@ -78,21 +94,33 @@ data MyList a = Empty | Cons a (MyList a)
 
 -- 25. Сделайте MyList представителем класса типов Eq.
 instance Eq a => Eq (MyList a) where
-    (==) = undefined
+    (==) Empty Empty               = True
+    (==) Empty _                   = False
+    (==) _ Empty                   = False
+    (==) (Cons a1 b1) (Cons a2 b2) = (a1 == a2) && (b1 == b2)
 
 -- 26. Сделайте MyList представителем класса типов Ord. Достаточно реализовать оператор (<=).
 instance Ord a => Ord (MyList a) where
-    (<=) = undefined
+    (<=) Empty Empty  = True
+    (<=) Empty _      = True
+    (<=) _ Empty      = False
+    (<=) (Cons a1 b1) (Cons a2 b2)
+          | a1 > a2   = False
+          | a1 < a2   = True
+          | otherwise = b1 <= b2
 
 -- 27. Сделайте MyList представителем класса типов Foldable.
 instance Foldable MyList where
-    foldr = undefined
+    foldr f init Empty      = init
+    foldr f init (Cons a b) = f a (foldr f init b)
 
 -- 28. Сделайте MyList представителем класса типов Functor.
 instance Functor MyList where
-    fmap = undefined
+    fmap f Empty      = Empty
+    fmap f (Cons a b) = Cons (f a) (fmap f b)
 
 -- 29. sum2D вычисляет сумму элементов двумерного списка.
 -- Используйте реализованные выше instance'ы, чтобы сделать все в бесточечном стиле.
+
 sum2D :: Num a => MyList (MyList a) -> a
-sum2D = undefined
+sum2D = sum . fmap sum
